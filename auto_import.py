cd954e0601a6568bd1df04b1959d36364a9ffb72
glaby823 <tomluc743@gmail.com>
2026-04-14 13:06:19 +0200

    p

--- auto_import.py
+++ auto_import.py
@@ -1,12 +1,4 @@
 import os
-
-commit=input("Nom du commit: ")
-
 os.system(f"git add .")
-os.system(f"git commit -m {commit}")
+os.system(f"git commit -m {input("Nom du commit: ")}")
 os.system(f"git push origin main")
-
-for i in range(10):
-    print("")
-
-print("Fini")
